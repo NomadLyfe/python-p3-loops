8f17905ac53bda6a6e894ce1f376fe0c23cea258
Jeremy Garcia <jeremygarcia9400@gmail.com>
2023-07-16 16:12:03 -0400

    complete lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i >= 0:
+        if i == 0:
+            print("Happy New Year!")
+        else:
+            print(i)
+        i -= 1
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    square_list = [num ** 2 for num in int_list]
+    return square_list
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(100):
+        if (i + 1) % 3 == 0 and (i + 1) % 5 == 0:
+            print('FizzBuzz')
+        elif (i + 1) % 3 == 0:
+            print('Fizz')
+        elif (i + 1) % 5 == 0:
+            print('Buzz')
+        else:
+            print(f"{i + 1}")
